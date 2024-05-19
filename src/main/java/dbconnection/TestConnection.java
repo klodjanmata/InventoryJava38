042c7fe2c94a799a4ba1f6e8ef03dbf68a2a1428
eljon11 <eljonkeci10@gmail.com>
2024-05-19 11:48:28 +0200

    Merge branch 'master' of https://github.com/klodjanmata/InventoryJava38

--- src/main/java/dbconnection/TestConnection.java
+++ src/main/java/dbconnection/TestConnection.java
@@ -1,7 +1,14 @@
 package dbconnection;
 
-public class TestConnection {
-    public static void main(String[] args) {
+import java.sql.*;
 
+public class TestConnection {
+    public static void main(String[] args) throws SQLException {
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/inventoryjava38", "root", "root");
+             Statement statement = connection.createStatement()) {
+
+            final ResultSet resultSet = statement.executeQuery("SELECT * FROM Category");
+        }
     }
 }
