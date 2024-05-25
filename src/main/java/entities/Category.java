d460f56f7e5d842814dcef445fed7422e22c5001
Bruno Hoxha <151655009+BrunoHoxha@users.noreply.github.com>
2024-05-25 11:09:49 +0200

    Done

--- src/main/java/entities/Category.java
+++ src/main/java/entities/Category.java
@@ -1,47 +1,25 @@
 package entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+import sun.util.resources.Bundles;
+
+@Entity
+@Table(name = "Category")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
 
-    public Category(int id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Category{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
 }
