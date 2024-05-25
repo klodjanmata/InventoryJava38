8d594a28af91d3494c16459d74321aeeb93a1831
MarjanRr <marjanrrusti@gmail.com>
2024-05-25 11:21:31 +0100

    Update Category.java

--- src/main/java/entities/Category.java
+++ src/main/java/entities/Category.java
@@ -1,47 +1,24 @@
 package entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Category")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Category {
+    @Id
+    @GeneratedValue
     private int id;
+
+@Column(name = "name")
     private String name;
+
+@Column(name = "description")
     private String description;
 
-    public Category(int id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Category{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
 }
